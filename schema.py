3def921405d5afe81b2ba5a9eea25ba6b4945d67
Joyce Namuli <joyce.namuli@andela.com>
2018-04-06 10:06:59 +0300

    Query users by id

--- schema.py
+++ schema.py
@@ -1,11 +1,11 @@
 from graphene import relay
 import graphene
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
-from models import User, Story
+from models import User as UserModel, Story
 
 class User(SQLAlchemyObjectType):
     class Meta:
-        model = User
+        model = UserModel
         interfaces = (relay.Node, )
 
 
@@ -17,7 +17,8 @@ class Query(graphene.ObjectType):
     node = relay.Node.Field()
     users = SQLAlchemyConnectionField(User)
     User_stories = SQLAlchemyConnectionField(Story)
+    find_user = graphene.relay.Node.Field(User)
     
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, types=[User,Story])
